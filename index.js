ed245dc39ea622f66dc61c97d2ccedcbf1fe3e49
LeFizzy <honterparade@gmail.com>
2018-09-19 22:11:11 +0200

    Remove unused dependency.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const googleTrends = require('google-trends-api');
 const gameShuffle = require('./lib/randomizeElements.js');
 const gameData = require('./lib/objectProvider.json');
 
@@ -15,4 +14,4 @@ app.get('/api/getgamedata', (req, res) => {
     res.send(shuffledData);
 });
 
-app.listen('8080', () => { console.log('Server started on 8080'); });
+app.listen('8080', () => { console.log('Server started on port 8080!'); });
